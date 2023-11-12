a5e85e0adb11cb55aba4a56fb5b68aa9e73ebcbe
Bernardo Moraes <bernardo.moraes.silva@gmail.com>
2023-11-12 20:59:05 -0300

    add logs and new verifications for orders service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -10,6 +10,49 @@ export class OrdersService {
   constructor(private readonly prisma: PrismaService) {}
   async create(input: CreateOrderDto) {
     try {
+      // verify if user exists
+      const user = await this.prisma.users.findUnique({
+        where: {
+          id: input.userId,
+        },
+      });
+
+      if (!user) {
+        throw new CustomError({
+          message: 'User not found',
+          status: 404,
+        });
+      }
+
+      if (input.items?.length === 0) {
+        throw new CustomError({
+          message: 'You must provide at least one item',
+          status: 400,
+        });
+      }
+
+      const products = await this.prisma.products.findMany({
+        where: {
+          id: {
+            in: input.items.map((item) => item.productId),
+          },
+        },
+      });
+
+      if (products.length !== input.items.length) {
+        const foundProducts = products.map((product) => product.id);
+        const notFoundProducts = input.items
+          .map((item) => item.productId)
+          .filter((productId) => !foundProducts.includes(productId));
+
+        throw new CustomError({
+          message: `The following products were not found: ${notFoundProducts.join(
+            ', ',
+          )}`,
+          status: 404,
+        });
+      }
+
       const response = await this.prisma.orders.create({
         data: {
           items: {
@@ -27,6 +70,7 @@ export class OrdersService {
           },
         },
       });
+      console.log(response);
       return response;
     } catch (error) {
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
@@ -36,23 +80,43 @@ export class OrdersService {
             message:
               'Something wrong with your input, please check the application logs for more details',
             status: 400,
-            error: error.message,
+            log: error.message,
           });
         }
         if (error.code === 'P2025') {
+          console.log(error);
           throw new CustomError({
-            message: error.name,
+            message: error.meta.cause as string,
             status: 404,
           });
         }
       }
+      if (error instanceof CustomError) {
+        throw error;
+      }
+      throw new CustomError({
+        message: 'Something went wrong during the database request',
+        status: 500,
+        log: error,
+      });
     }
   }
 
   async findAll() {
     return await this.prisma.orders.findMany({
       include: {
-        items: true,
+        items: {
+          include: {
+            product: true,
+          },
+        },
+        user: {
+          select: {
+            id: true,
+            full_name: true,
+            email: true,
+          },
+        },
       },
     });
   }
